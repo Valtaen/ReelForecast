1bd3e08222e658816c8e90b4530064e3adbe11b6
Matt Stewart <stewartms2@g.cofc.edu>
2024-02-22 16:38:36 -0500

    playing around with starting server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
   
   const express = require('express')
   const app = express()
+  const expressLayouts = require('express-ejs-layouts')
   const bcrypt = require('bcrypt')
   const passport = require('passport')
   const flash = require('express-flash')
@@ -17,9 +18,21 @@ if (process.env.NODE_ENV !== 'production') {
     id => users.find(user => user.id === id)
   )
   
+  const mongoose = require('mongoose')
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
+
   const users = []
   
   app.set('view-engine', 'ejs')
+  app.set('view engine', 'ejs')
+  app.set('views', __dirname + '/views')
+  app.set('layout', 'layouts/layout')
+  app.use(expressLayouts)
+  app.use(express.static('public'))
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
   app.use(session({
