475a95468959f9161e06dfef361916e90d5d3fce
Matt Stewart <89479382+Valtaen@users.noreply.github.com>
2024-03-15 22:02:00 -0400

    Add files via upload

--- dbConfig.js
+++ dbConfig.js
@@ -2,20 +2,11 @@ require("dotenv").config();
 
 const { Pool } = require('pg');
 
-// const isProduction = process.env.NODE_ENV === "production";
-
-// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-// const pool = new Pool({
-//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//   ssl: isProduction
-// });
-
 const pool = new Pool({
-  user: 'matt',
-  host: 'localhost',
-  database: 'reelforecast',
-  password: 'Ereth242',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: 3000,
   idleTimeoutMillis: 300000,
   connectionTimeoutMillis: 300000,
